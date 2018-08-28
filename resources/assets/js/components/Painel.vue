305959f666fc02250f2363eef6d28f3808758e7d
Rafael Doria <ieieldoria@hotmail.com>
2018-08-28 18:47:17 -0300

    Primeiros elementos vue.

---
+++ resources/assets/js/components/Painel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-bind:class="defineCor">
+        <div class="panel-heading">{{titulo}}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['titulo','cor'],
+        computed:{
+            defineCor: function(){
+                return "panel "+ (this.cor || "panel-default");
+            }
+        }
+    }
+</script>
+
+<style media="screen">
+    .blue {
+        border-color: #0d4a96
+    }
+    .blue > .panel-heading{
+        color: #f7f7f7;
+        background-color: #0d4a96;
+        border-color: #0d4a96
+    }
+
+    .red {
+        border-color: #520b28
+    }
+    .red > .panel-heading{
+        color: #f7f7f7;
+        background-color: #520b28;
+        border-color: #520b28
+    }
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Dashboard">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4">
+                            <painel titulo="Cont1" cor="blue">
+                                Usuários
+                            </painel>
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                        <div class="col-md-4 col-sm-4">
+                            <painel titulo="Cont2" cor="red">
+                                Usuários
+                            </painel>
+                        </div>
+                        <div class="col-md-4 col-sm-4">
+                            <painel titulo="Cont3" cor="panel-success">
+                                Usuários
+                            </painel>
+                        </div>
+                    </div>
+                </painel>
             </div>
         </div>
     </div>
-</div>
 @endsection
